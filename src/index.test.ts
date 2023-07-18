2179733c0363337774ec7e70484c161a57bf2154
alan <alan@fanruan.com>
2023-07-18 17:10:55 +0800

    init

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { isEquals } from ".";
+
+it("测试用例", () => {
+  expect(isEquals(1, 1)).toBe(true);
+});
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isEquals(a: any, b: any): boolean {
+  return a === b;
+}
