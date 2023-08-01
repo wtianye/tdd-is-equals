875bf66061c819a90996eb420eae1da7d6dc7b5b
Henry.Wang <Henry.Wang@fanruan.com>
2023-08-01 09:38:34 +0800

    feat: 提交代码 #none

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,40 @@
 import { isEquals } from ".";
 
-it("测试用例", () => {
-  expect(isEquals(1, 1)).toBe(true);
+describe("isEquals测试", () => {
+  it("基本", () => {
+    expect(isEquals(1, 1)).toBe(true);
+    expect(isEquals(1, 2)).toBe(false);
+    expect(isEquals(false, false)).toBe(true);
+    expect(isEquals("1", "1")).toBe(true);
+    expect(isEquals(null, null)).toBe(true);
+    expect(isEquals(undefined, undefined)).toBe(true);
+  });
+
+  it("类型不同", () => {
+    expect(isEquals(1, "1")).toBe(false);
+    expect(isEquals(null, undefined)).toBe(false);
+    expect(isEquals(0, undefined)).toBe(false);
+    expect(isEquals(0, null)).toBe(false);
+    expect(isEquals(0, false)).toBe(false);
+    expect(isEquals(1, true)).toBe(false);
+    expect(isEquals(null, NaN)).toBe(false);
+    expect(isEquals(undefined, NaN)).toBe(false);
+  });
+
+  it("NaN相关", () => {
+    expect(isEquals(NaN, NaN)).toBe(true);
+  });
+
+  it("0相关", () => {
+    expect(isEquals(-0, +0)).toBe(true);
+    expect(isEquals(0, +0)).toBe(true);
+    expect(isEquals(-0, 0)).toBe(true);
+  });
+
+  it("对象相关", () => {
+    expect(isEquals({ a: 1 }, { a: 1 })).toBe(true);
+    expect(isEquals([1, 2, 3], [1, 2, 3])).toBe(true);
+    expect(isEquals([1, 2, "3"], [1, 2, 3])).toBe(false);
+    expect(isEquals({ a: 1, b: [10, 20] }, { a: 1, b: [10, 20] })).toBe(true);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+import { isEqual } from "lodash";
+
 export function isEquals(a: any, b: any): boolean {
-  return a === b;
+  return isEqual(a, b);
 }
